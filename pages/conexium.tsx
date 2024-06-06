080495ac3de958cbdc2f6a00a69273fb0ec2cdaf
brotherSam <samuelobior@yahoo.co.uk>
2024-06-06 21:05:27 +0100

    case test1

--- pages/conexium.tsx
+++ pages/conexium.tsx
@@ -4,19 +4,46 @@ import Image from "next/image";
 
 function conexium() {
   return (
-<main className="flex items-center justify-center flex-col w-full bg-[#121212] px-4 md:px-8">
-  
-    <Image
-      className="w-full md:w-[90%] h-auto px-4 md:px-20"
-      loading="lazy"
-      src="/img/conexium.jpg"
-      alt="conexium case study"
-      width={1000}
-      height={0}
-    />
+    <main className="flex items-center justify-center flex-col w-full !bg-[#121212] px-4 md:px-8">
+      <div className="flex flex-col items-center justify-center">
+        <Image
+          className="w-full md:w-[90%] px-4 md:px-20"
+          loading="lazy"
+          src="/img/conexium.png"
+          alt="conexium case study"
+          width={1000}
+          height={0}
+        />
+        <hr
+          className="h-[0.5px] w-[50%] my-4 bg-gray-200 
+              border-0 dark:bg-gray-700"
+        ></hr>
+        <Image
+          className="w-full md:w-[90%] px-4 md:px-20"
+          loading="lazy"
+          src="/img/overview.png"
+          alt="conexium case study"
+          width={1000}
+          height={0}
+        />
 
-  <Footer/>
-</main>
+        <hr
+          className="h-[0.5px] w-[50%] my-4 bg-gray-200 
+              border-0 dark:bg-gray-700"
+        ></hr>
+
+        <Image
+          className="w-full md:w-[90%] px-4 md:px-20"
+          loading="lazy"
+          src="/img/conexium.png"
+          alt="conexium case study"
+          width={1000}
+          height={0}
+        />
+      </div>
+
+      <Footer />
+    </main>
   );
 }
 
