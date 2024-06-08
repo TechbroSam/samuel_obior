aff4344177679c853f2916880562b866b2c43310
brotherSam <samuelobior@yahoo.co.uk>
2024-06-08 15:46:45 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="font-medium text-[0.8rem] md:text-lg flex items-center w-full px-4 md:px-20 lg:px-60 py-20 md:py-20 justify-center">
+    <div className="font-normal text-center text-sm md:text-lg flex items-center w-full px-4 md:px-20 lg:px-60 py-20 md:py-20 justify-center">
      <p>&#169; Copyright 2024. Samuel Obior | All Rights Reserved</p>
     </div>
   );
